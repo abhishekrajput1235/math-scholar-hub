328727203a1c8741f5d3fa402076f99ef53ef7df
abhishekrajput1235 <abhishekrajput.bca2k22@mpgi.edu.in>
2026-01-29 21:55:53 +0530

    Fix scroll to top on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -15,9 +15,12 @@ import { useEffect } from "react";
 
 // Scroll to top on route change
 function ScrollToTop() {
+  const [location] = useLocation();
+  
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+    window.scrollTo({ top: 0, behavior: "instant" });
+  }, [location]);
+  
   return null;
 }
 
